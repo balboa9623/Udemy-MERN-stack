e8458ad55f0106e785bd9f48c29d9bccb38cafb7
Richu Mathew <richu.a.mathew@my.occc.edu>
2021-06-24 00:21:15 -0500

    created config file for database connection and mongoURL connection string. Also created files for routes.

--- DevConnect/server.js
+++ DevConnect/server.js
@@ -1,8 +1,18 @@
 const express = require('express');
+const connectDB = require('./config/db'); // connect the db file
 
 const app = express();
 
+// Connect Database
+connectDB();
+
 app.get('/', (req, res) => res.send('API Running')); // send data to the browser
+
+// Define Routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/posts', require('./routes/api/posts'));
+app.use('/api/profile', require('./routes/api/profile'));
 
 const PORT = process.env.PORT || 5000; //process.env.PORT looks for an environment variable called PORT when we deploy to Heroku. When we run locally it runs on port 5000
 
